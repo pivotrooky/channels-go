1c4875586d79f5ced3df1adcc777f7a61594f8a0
pivotrooky <towagner@gmail.com>
2021-07-31 01:50:42 -0300

    Infinite loop

--- main.go
+++ main.go
@@ -8,17 +8,25 @@ import (
 func main() {
 	links := []string{"http://google.com", "http://facebook.com", "http://stackoverflow.com", "http://golang.org", "http://amazon.com"}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go checkLink(l, c)
 	}
 
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- link
 		return
 	}
 	fmt.Println(link, "is ok")
+	c <- link
 }
